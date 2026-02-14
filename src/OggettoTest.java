42a0b3ed5feab8aed402d7b40f3726d631c72911
balabioluca <luca.ballabio@outlook.com>
2026-02-14 12:36:15 +0100

    Addded tests

---
+++ src/OggettoTest.java
@@ -0,0 +1,7 @@
+import static org.junit.jupiter.api.Assertions.*;
+class OggettoTest {
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
--- src/Persona.java
+++ src/Persona.java
@@ -14,4 +14,6 @@ public class Persona {
                 ", cognome='" + cognome + '\'' +
                 '}';
     }
+
+
 }
