72fb6daab82ca5a4ae252052c579761a338c718f
balabioluca <luca.ballabio@outlook.com>
2026-02-14 12:04:13 +0100

    Created obj's in Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to this test");
-
+            Persona p1 = new Persona("Luca","");
+            Persona p2 = new Persona("Jerry","Scott");
+            Persona p3 = new Persona("Fab","Fib");
 
     }
 }
