cc961c49b8d73edc377ac54d2053df64e90a8484
balabioluca <luca.ballabio@outlook.com>
2026-02-14 12:16:28 +0100

    Added class Constructor and get/set methods

--- src/Oggetto.java
+++ src/Oggetto.java
@@ -1,3 +1,25 @@
 public class Oggetto {
+    private String nome;
+    private String peso;
 
+    public Oggetto(String nome, String peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
 }
