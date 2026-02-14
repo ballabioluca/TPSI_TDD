f127c126103fab10b06c72b7f178218fc366e774
balabioluca <luca.ballabio@outlook.com>
2026-02-14 12:06:06 +0100

    Fixed #4 Closed #4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to this test");
-            Persona p1 = new Persona("Luca","");
-            Persona p2 = new Persona("Jerry","Scott");
-            Persona p3 = new Persona("Fab","Fib");
+            Persona p1 = new Persona("Luca","Ballabio");
+            Persona p2 = new Persona("Gerry","Scotti");
+            Persona p3 = new Persona("Fabri","Fibra");
 
     }
 }
