b2862d3644e85d3e53433ac5d2a3a37712630922
balabioluca <luca.ballabio@outlook.com>
2026-02-14 12:11:40 +0100

    Added printing functionality

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to this test");
-            Persona p1 = new Persona("Luca","Ballabio");
-            Persona p2 = new Persona("Gerry","Scotti");
-            Persona p3 = new Persona("Fabri","Fibra");
+        Persona p1 = new Persona("Luca","Ballabio");
+        Persona p2 = new Persona("Gerry","Scotti");
+        Persona p3 = new Persona("Fabri","Fibra");
 
+        System.out.println("Persona 1: "+p1);
+        System.out.println("Persona 2: "+p3);
+        System.out.println("Persona 3: "+p2);
     }
 }
